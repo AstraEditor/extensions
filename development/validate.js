b8f05e9559fc4c0e2407b917ab86daa66c9e66b5
KOSHINOawa <2381068747@qq.com>
2025-11-16 22:03:34 +0800

    Initial commit: TurboWarp Extensions

---
+++ development/validate.js
@@ -0,0 +1,24 @@
+import Builder from "./builder.js";
+import * as Colors from "./colors.js";
+
+const builder = new Builder("production");
+const errors = await builder.validate();
+
+if (errors.length === 0) {
+  console.log(
+    `${Colors.GREEN}${Colors.BOLD}Validation checks passed.${Colors.RESET}`
+  );
+  process.exit(0);
+} else {
+  console.error(
+    `${Colors.RED}${Colors.BOLD}${errors.length} ${
+      errors.length === 1 ? "file" : "files"
+    } failed validation.${Colors.RESET}`
+  );
+  console.error("");
+  for (const { fileName, error } of errors) {
+    console.error(`${Colors.BOLD}${fileName}${Colors.RESET}: ${error}`);
+  }
+  console.error(``);
+  process.exit(1);
+}
